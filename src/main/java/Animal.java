95c56e69fd9704d083dd8770c6781d193c46caa9
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-09-30 07:33:25 -0700

    implement all and save methods, along with equals override.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -10,7 +10,6 @@ public class Animal {
 
   public Animal(String name) {
     this.name = name;
-
   }
 
   public String getName() {
@@ -21,15 +20,34 @@ public class Animal {
     return ENDANGERED;
   }
 
-  // public List<Animal> all() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "SELECT * FROM animals;"
-  //     return con.createQuery(sql).executeAndFetch(Animal.class);
-  //   }
-  // }
+  public static List <Animal> all() {
+    String sql = "SELECT * FROM animals";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).throwOnMappingFailure(false).executeAndFetch(Animal.class);
+    }
+  }
 
-  // public void save() {
-  //   try Connection
-  // }
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered)";
+      this.id = (int) con.createQuery(sql, true)
+        .throwOnMappingFailure(false)
+        .addParameter("name", this.name)
+        .addParameter("endangered", ENDANGERED)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherAnimal) {
+    if (!(otherAnimal instanceof Animal)) {
+      return false;
+    } else {
+      Animal newAnimal = (Animal) otherAnimal;
+      return this.getName().equals(newAnimal.getName()) &&
+        this.isEndangered() == newAnimal.isEndangered();
+    }
+  }
 
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
-
+//Create .equals override
 public class AnimalTest {
 
   @Rule
@@ -25,20 +25,29 @@ public class AnimalTest {
     assertEquals(false, testAnimal.isEndangered());
   }
 
-  // @Test
-  // public void all_returnsAllInstancesOfAnimal_true() {
-  //   Animal firstAnimal = new Animal("Bear");
-  //   firstAnimal.save();
-  //   Animal secondAnimal = new Animal("Chipmunk");
-  //   secondAnimal.save();
-  //   assertTrue(Animal.all().get(0).equals(firstAnimal));
-  //   assertTrue(Animal.all().get(1).equals(secondAnimal));
-  // }
-  //
-  // @Test
-  // public void save_successfullyAddsAnimalToDatabase_True() {
-  //   Animal testAnimal = new Animal("Bear");
-  //   testAnimal.save();
-  //   assertTrue(Animal.all().get(0).equals(testAnimal));
-  // }
+  @Test
+  public void equals_recognizesSameValues_true () {
+    Animal animalOne = new Animal("Bear");
+    animalOne.save();
+    Animal animalTwo = new Animal("Bear");
+    animalTwo.save();
+    assertEquals(true, animalOne.equals(animalTwo));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfAnimal_true() {
+    Animal firstAnimal = new Animal("Bear");
+    firstAnimal.save();
+    Animal secondAnimal = new Animal("Chipmunk");
+    secondAnimal.save();
+    assertTrue(Animal.all().get(0).equals(firstAnimal));
+    assertTrue(Animal.all().get(1).equals(secondAnimal));
+  }
+
+  @Test
+  public void save_successfullyAddsAnimalToDatabase_True() {
+    Animal testAnimal = new Animal("Bear");
+    testAnimal.save();
+    assertTrue(Animal.all().get(0).equals(testAnimal));
+  }
 }
