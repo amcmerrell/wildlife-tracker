8a0873b7208b78bdbffb28dca63da92b1ef116a1
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-09-29 20:42:32 -0700

    add name getter, along with passing test.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -4,12 +4,27 @@ import java.util.List;
 public class Animal {
   public int id;
   public String name;
-  public boolean endangered;
+  //public boolean endangered;
 
   public static final boolean ENDANGERED = false;
 
   public Animal(String name) {
     this.name = name;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  // public List<Animal> all() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM animals;"
+  //     return con.createQuery(sql).executeAndFetch(Animal.class);
+  //   }
+  // }
+
+  // public void save() {
+  //   try Connection
+  // }
 
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -9,7 +9,30 @@ public class AnimalTest {
 
   @Test
   public void constructor_animalInstantiatesCorrectly_true() {
-    Animal newAnimal = new Animal("Bear");
+    Animal testAnimal = new Animal("Bear");
     assertEquals(true, newAnimal instanceof Animal);
   }
+
+  @Test
+  public void getName_getsCorrectName() {
+    Animal testAnimal = new Animal("Bear");
+    assertEquals("Bear", newAnimal.getName());
+  }
+
+  // @Test
+  // public void all_returnsAllInstancesOfAnimal_true() {
+  //   Animal firstAnimal = new Animal("Bear");
+  //   firstAnimal.save();
+  //   Animal secondAnimal = new Animal("Chipmunk");
+  //   secondAnimal.save();
+  //   assertTrue(Animal.all().get(0).equals(firstAnimal));
+  //   assertTrue(Animal.all().get(1).equals(secondAnimal));
+  // }
+  //
+  // @Test
+  // public void save_successfullyAddsAnimalToDatabase_True() {
+  //   Animal testAnimal = new Animal("Bear");
+  //   newAnimal.save();
+  //   assertTrue(Animal.all().get(0).equals(testMonster));
+  // }
 }
